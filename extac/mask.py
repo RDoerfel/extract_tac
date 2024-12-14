72f17a09823937b0a70e8dbcf3182c9831ae04f1
RDoerfel <doerfelruben@aol.com>
2024-12-14 13:28:41 +0100

    [enh] add roi extraction and tests

--- extac/mask.py
+++ extac/mask.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def create_mask_from_indices(original_mask, roi_indices):
+def _create_roi_mask_from_indices(original_mask, roi_indices):
     """
     Create a boolean mask of the same shape as the original array,
     with True where values are in the list, False otherwise.
@@ -19,9 +19,41 @@ def create_mask_from_indices(original_mask, roi_indices):
         Boolean mask with the same shape as original_array
     """
     # Create a mask initialized with False
-    mask = np.zeros(original_mask.shape, dtype=bool)
+    roi_mask = np.zeros(original_mask.shape, dtype=bool)
 
     # Set True for elements that are in the value list
-    mask[np.isin(original_mask, roi_indices)] = True
+    roi_mask[np.isin(original_mask, roi_indices)] = True
 
-    return mask
+    return roi_mask
+
+
+def _get_values_in_roi(roi: np.array, image: np.array) -> np.array:
+    roi_values = image[roi]
+    return roi_values
+
+
+def _get_mean_from(masked_values: np.array) -> float:
+    roi_mean = masked_values.mean()
+    return roi_mean
+
+
+def extract_roi_mean(region_masks: np.array, image: np.array, roi_index: list) -> float:
+    """Extract the mean value in the image within the region of interest defined by the roi_index
+    Parameters:
+    -----------
+    region_masks : numpy.ndarray
+        The mask image
+    image : numpy.ndarray
+        The image to extract the mean value from
+    roi_index : list
+        List of integer values to match
+
+    Returns:
+    --------
+    float
+        Mean value in the image within the region of interest
+    """
+    roi_mask = _create_roi_mask_from_indices(region_masks, roi_index)
+    roi_values = _get_values_in_roi(roi_mask, image)
+    roi_mean = _get_mean_from(roi_values)
+    return roi_mean
--- extac/tests/test_mask.py
+++ extac/tests/test_mask.py
@@ -1,26 +1,29 @@
-from extac.mask import create_mask_from_indices
+from extac.mask import _create_roi_mask_from_indices
+from extac.mask import _get_values_in_roi
+from extac.mask import _get_mean_from
+from extac.mask import extract_roi_mean
 import numpy as np
 
 
 def test_create_mask_from_indices():
     original_mask = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     roi_indices = [1, 2, 3]
-    mask = create_mask_from_indices(original_mask, roi_indices)
+    mask = _create_roi_mask_from_indices(original_mask, roi_indices)
     expected_mask = np.array([[True, True, True], [False, False, False], [False, False, False]])
     np.testing.assert_array_equal(mask, expected_mask)
 
     roi_indices = [2, 3, 4]
-    mask = create_mask_from_indices(original_mask, roi_indices)
+    mask = _create_roi_mask_from_indices(original_mask, roi_indices)
     expected_mask = np.array([[False, True, True], [True, False, False], [False, False, False]])
     np.testing.assert_array_equal(mask, expected_mask)
 
     roi_indices = [5, 6, 7]
-    mask = create_mask_from_indices(original_mask, roi_indices)
+    mask = _create_roi_mask_from_indices(original_mask, roi_indices)
     expected_mask = np.array([[False, False, False], [False, True, True], [True, False, False]])
     np.testing.assert_array_equal(mask, expected_mask)
 
     roi_indices = [8, 9]
-    mask = create_mask_from_indices(original_mask, roi_indices)
+    mask = _create_roi_mask_from_indices(original_mask, roi_indices)
     expected_mask = np.array([[False, False, False], [False, False, False], [False, True, True]])
     np.testing.assert_array_equal(mask, expected_mask)
 
@@ -28,7 +31,7 @@ def test_create_mask_from_indices():
 def test_create_mask_from_indices_empty():
     original_mask = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     roi_indices = []
-    mask = create_mask_from_indices(original_mask, roi_indices)
+    mask = _create_roi_mask_from_indices(original_mask, roi_indices)
     expected_mask = np.array([[False, False, False], [False, False, False], [False, False, False]])
     np.testing.assert_array_equal(mask, expected_mask)
 
@@ -36,6 +39,48 @@ def test_create_mask_from_indices_empty():
 def test_create_mask_from_indices_no_match():
     original_mask = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     roi_indices = [10, 11, 12]
-    mask = create_mask_from_indices(original_mask, roi_indices)
+    mask = _create_roi_mask_from_indices(original_mask, roi_indices)
     expected_mask = np.array([[False, False, False], [False, False, False], [False, False, False]])
     np.testing.assert_array_equal(mask, expected_mask)
+
+
+def test_mask_image():
+    mask = np.array([[True, True, True], [False, False, False], [False, False, False]])
+    image = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    masked_image = _get_values_in_roi(mask, image)
+    expected_masked_image = np.array([1, 2, 3])
+    np.testing.assert_array_equal(masked_image, expected_masked_image)
+
+    mask = np.array([[False, True, True], [True, False, False], [False, False, False]])
+    masked_image = _get_values_in_roi(mask, image)
+    expected_masked_image = np.array([2, 3, 4])
+    np.testing.assert_array_equal(masked_image, expected_masked_image)
+
+
+def test_mask_image_empty():
+    mask = np.array([[False, False, False], [False, False, False], [False, False, False]])
+    image = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    masked_image = _get_values_in_roi(mask, image)
+    expected_masked_image = np.array([])
+    np.testing.assert_array_equal(masked_image, expected_masked_image)
+
+
+def test_get_mean_from():
+    masked_image = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    roi_mean = _get_mean_from(masked_image)
+    expected_roi_mean = masked_image.mean()
+    assert roi_mean == expected_roi_mean
+
+    masked_image = np.array([0, 0])
+    roi_mean = _get_mean_from(masked_image)
+    expected_roi_mean = masked_image.mean()
+    assert roi_mean == expected_roi_mean
+
+
+def test_extract_roi_mean():
+    roi_mask = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    image = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    roi_index = [1, 4, 7]
+    roi_mean = extract_roi_mean(roi_mask, image, roi_index)
+    expected_roi_mean = np.array([1, 4, 7]).mean()
+    assert roi_mean == expected_roi_mean
