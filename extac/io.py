1783547551c106c3a0c244c729e5bff48efb9789
RDoerfel <doerfelruben@aol.com>
2024-12-14 17:16:12 +0100

    [enh] add function to write results to tsv file to io module

--- extac/io.py
+++ extac/io.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import nibabel as nib
+import pandas as pd
 import json as js
 
 
@@ -11,3 +12,7 @@ def read_js(js_file: Path):
     with open(js_file, "r") as f:
         rois = js.load(f)
     return rois
+
+
+def write_tsv(df: pd.DataFrame, tsv_path: Path):
+    df.to_csv(tsv_path, sep="\t", index=False)
