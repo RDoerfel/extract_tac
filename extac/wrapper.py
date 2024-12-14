d45d911d3c7561f35667fa188436c9e1f348b93c
RDoerfel <doerfelruben@aol.com>
2024-12-14 17:37:21 +0100

    [enh]: add extract_tacts conventient function to wrapper

--- extac/wrapper.py
+++ extac/wrapper.py
@@ -1,17 +1,36 @@
-import numpy as np
-import nibabel as nib
-from extac.mask import get_tac_from_roi, get_mean_from_roi
-from extac.utils import get_nibimage_data
+from extac.mask import get_values_for_roi
+from extac import utils
+from extac import io
+from pathlib import Path
 
 
-def get_values_for_roi(
-    image: nib.Nifti1Image, mask: nib.Nifti1Image, roi_indices: list, dynamic: bool = False
-) -> np.array:
-    image_data = get_nibimage_data(image)
-    mask_data = get_nibimage_data(mask)
-    if dynamic:
-        values = get_tac_from_roi(image_data, mask_data, roi_indices)
-    else:
-        values = get_mean_from_roi(image_data, mask_data, roi_indices)
+def extract_tacs(image_file: Path, mask_file: Path, roi_file: Path, output_file: Path, dynamic: bool = True):
+    """Extract TACs from an image using a mask and ROIs
+    Parameters
+    ----------
+        image_file: Path
+            Path to the image file
+        mask_file: Path
+            Path to the mask file
+        roi_file: Path
+            Path to the ROI file
+        output_file: Path
+            Path to the output file
+        dynamic: bool
+            If True, extract TACs. If False, extract mean values
+    """
 
-    return values
+    image = io.load_image(image_file)
+    mask = io.load_image(mask_file)
+    rois = io.read_js(roi_file)
+    mask_resampled = utils.resample_from_to(mask, image)
+    image_data = utils.get_nibimage_data(image)
+    mask_data = utils.get_nibimage_data(mask_resampled)
+    extracted_values = []
+    for roi in rois:
+        roi_values = get_values_for_roi(image_data, mask_data, roi["index"], dynamic=dynamic)
+        result_dict = {roi["name"]: roi_values}
+        extracted_values.append(result_dict)
+
+    results = utils.convert_list_of_dicts_to_dataframe(extracted_values)
+    io.write_tsv(results, output_file)
