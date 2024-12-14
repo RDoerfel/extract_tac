cbaa0c51a353c1bc8020221da3f680520f92f395
RDoerfel <doerfelruben@aol.com>
2024-12-14 18:50:12 +0100

    [maint] supress deprecation warning for resamplig

---
+++ extac/cli.py
@@ -0,0 +1,22 @@
+import argparse
+from pathlib import Path
+from wrapper import extract_tacs
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Extract TACs or mean values from images")
+    parser.add_argument("--image", type=Path, required=True, help="Path to the cerebellum segmentation file")
+    parser.add_argument("--mask", type=Path, required=True, help="Path to the brain segmentation file")
+    parser.add_argument("--rois", type=Path, required=True, help="Path to the output reference region file")
+    parser.add_argument("--output", type=Path, required=True, help="Path to the output file")
+    parser.add_argument(
+        "--dynamic",
+        action="store_true",
+        help="If True, extract TACs. If False, extract mean values",
+    )
+    args = parser.parse_args()
+    extract_tacs(args.image, args.mask, args.rois, args.output, args.dynamic)
+
+
+if __name__ == "__main__":
+    main()
--- extac/utils.py
+++ extac/utils.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 
 def resample_from_to(image_from: nib.Nifti1Image, image_to: nib.Nifti1Image) -> nib.Nifti1Image:
-    return resample_to_img(image_from, image_to, interpolation="nearest", copy_header=True)
+    return resample_to_img(image_from, image_to, interpolation="nearest", copy_header=True, force_resample=True)
 
 
 def get_nibimage_data(nibimage: nib.Nifti1Image) -> np.array:
