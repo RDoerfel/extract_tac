910b7e632a12155036a84098aa67924ac3e5fe0a
RDoerfel <doerfelruben@aol.com>
2024-12-18 12:48:42 +0100

    [enh] add support for multiple measures to extract

--- extac/cli.py
+++ extac/cli.py
@@ -10,12 +10,20 @@ def main():
     parser.add_argument("--rois", type=Path, required=True, help="Path to the output reference region file")
     parser.add_argument("--output", type=Path, required=True, help="Path to the output file")
     parser.add_argument(
+        "--measure",
+        type=str,
+        nargs="+",
+        default=["mean", "median", "std", "count"],
+        choices=["mean", "median", "std", "count"],
+        help="One or more measures to extract",
+    )
+    parser.add_argument(
         "--dynamic",
         action="store_true",
         help="If True, extract TACs. If False, extract mean values",
     )
     args = parser.parse_args()
-    extract_tacs(args.image, args.mask, args.rois, args.output, args.dynamic)
+    extract_tacs(args.image, args.mask, args.rois, args.output, args.measure, args.dynamic)
 
 
 if __name__ == "__main__":
